6d2570da0cd8c6da69f5b7986a9bc059717b4c5f
Zaki <zakislaboratory@gmail.com>
2023-11-17 09:11:03 -0600

    convert the body to simple markdown

--- cmd/main.go
+++ cmd/main.go
@@ -6,10 +6,14 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"regexp"
+	"strconv"
 	"strings"
+	"time"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
-// RSSItem represents the structure of an RSS item
 type RSSItem struct {
 	Title       string `xml:"title"`
 	Description string `xml:"description"`
@@ -60,30 +64,115 @@ func main() {
 
 	// Process each RSS item
 	for _, item := range rss.Items {
+
+		content := stripClassesFromHTML(item.Content)
+		content = stripStylesFromHTML(content)
+		content, err = convertHTMLToMarkdownSimple(content)
+		if err != nil {
+			fmt.Printf("Error converting HTML to Markdown: %v\n", err)
+			return
+		}
+
+		date, err := convertDateFormat(item.PubDate)
+		if err != nil {
+			fmt.Printf("Error converting date format: %v\n", err)
+			return
+		}
+
 		mdxContent := fmt.Sprintf(`---
 title: "%s"
 description: "%s"
 link: "%s"
 guid: "%s"
-pubDate: "%s"
+date: "%s"
+lang: "en"
+tags: ["Personal Development", "Systems"]
+category: "Growth"
 creator: "%s"
 ---
 
 %s
-`, item.Title, item.Description, item.Link, item.GUID, item.PubDate, item.Creator, item.Content)
+`, item.Title, item.Description, item.Link, item.GUID,
+			date, item.Creator, content)
 
 		// Clean up title for filename
 		filename := cleanFilename(item.Title) + ".mdx"
 
 		// Write MDX content to file
 		outputPath := filepath.Join(*outputDir, filename)
-		err := os.WriteFile(outputPath, []byte(mdxContent), os.ModePerm)
+		err = os.WriteFile(outputPath, []byte(mdxContent), os.ModePerm)
 		if err != nil {
 			fmt.Printf("Error writing MDX file %s: %v\n", filename, err)
 		} else {
 			fmt.Printf("MDX file %s created successfully\n", filename)
 		}
 	}
+}
+
+// convertHTMLToMarkdownSimple converts HTML to a simplified Markdown format.
+func convertHTMLToMarkdownSimple(htmlContent string) (string, error) {
+	// Remove classes and styles from HTML
+	htmlContent = stripClassesFromHTML(htmlContent)
+	htmlContent = stripStylesFromHTML(htmlContent)
+
+	// Load HTML content into a goquery document
+	doc, err := goquery.NewDocumentFromReader(strings.NewReader(htmlContent))
+	if err != nil {
+		return "", err
+	}
+	// Convert HTML elements to Markdown
+	var markdownContent string
+	doc.Find("body *").Each(func(_ int, s *goquery.Selection) {
+		switch s.Get(0).Data {
+		case "p":
+			markdownContent += "\n\n" + s.Text() + "\n\n"
+		case "h1", "h2", "h3", "h4", "h5", "h6":
+			level, _ := strconv.Atoi(s.AttrOr("level", "2"))
+			markdownContent += fmt.Sprintf("\n\n%s %s\n\n", strings.Repeat("#", level), s.Text())
+		case "a":
+			href, _ := s.Attr("href")
+			markdownContent += fmt.Sprintf("[%s](%s)", s.Text(), href)
+		case "ul":
+			markdownContent += "\n"
+			s.Find("li").Each(func(_ int, li *goquery.Selection) {
+				markdownContent += fmt.Sprintf("* %s\n", li.Text())
+			})
+			markdownContent += "\n"
+		case "ol":
+			markdownContent += "\n"
+			s.Find("li").Each(func(i int, li *goquery.Selection) {
+				markdownContent += fmt.Sprintf("%d. %s\n", i+1, li.Text())
+			})
+			markdownContent += "\n"
+		default:
+			// markdownContent += "\n" + s.Text() + "\n"
+		}
+	})
+
+	// Define the regex pattern for the "Powered by beehiiv" line
+	poweredByBeehiivPattern := `\[[^\]]+\]\(https:\/\/www\.beehiiv\.com\/\?.*?\)`
+
+	// Compile the regex pattern
+	re := regexp.MustCompile(poweredByBeehiivPattern)
+
+	// Strip "Powered by beehiiv" at the end of the content
+	markdownContent = re.ReplaceAllString(markdownContent, "")
+
+	return markdownContent, nil
+}
+
+func stripClassesFromHTML(htmlContent string) string {
+	// Remove class attributes from HTML elements
+	// i.e. : <div class='beehiiv'> or <div class="beehiiv"> => <div>
+	re := regexp.MustCompile(` class=['"][^'"]*['"]`)
+	return re.ReplaceAllString(htmlContent, "")
+}
+
+func stripStylesFromHTML(htmlContent string) string {
+	// Remove style attributes from HTML elements
+	// i.e. : <div style='color: red;'> or <div style="color: red;"> => <div>
+	re := regexp.MustCompile(` style=['"][^'"]*['"]`)
+	return re.ReplaceAllString(htmlContent, "")
 }
 
 // cleanFilename removes invalid characters from a string to be used as a filename
@@ -98,4 +187,17 @@ func cleanFilename(s string) string {
 	s = strings.Trim(s, "_")
 	s = strings.ToLower(s)
 	return s
+}
+
+func convertDateFormat(inputDate string) (string, error) {
+	// Parse the input date string
+	parsedTime, err := time.Parse("Mon, 02 Jan 2006 15:04:05 -0700", inputDate)
+	if err != nil {
+		return "", err
+	}
+
+	// Format the date in the desired output format
+	outputDate := parsedTime.Format("2006-01-02")
+
+	return outputDate, nil
 }
