74cbca11a1d9bec6f4ea042dd57a9a1a820ff816
Zaki <zakislaboratory@gmail.com>
2023-11-17 14:05:52 -0600

    anchor test fix

--- pkg/markdown/anchor_test.go
+++ pkg/markdown/anchor_test.go
@@ -12,15 +12,12 @@ func TestConvertAnchor(t *testing.T) {
 	tcs := []struct {
 		name    string
 		rawHTML string
-		want    Link
+		want    string
 	}{
 		{
 			name:    "test 1",
 			rawHTML: `<a class="link" href="https://soundcloud.com/zakimusicofficial/tracks?utm_source=circadiangrowth.beehiiv.com&utm_medium=newsletter&utm_campaign=melodic-mastery-4-ways-music-can-improve-our-lives" target="_blank" rel="noopener noreferrer nofollow">my own electronic music</a> `,
-			want: Link{
-				Text: "my own electronic music",
-				Href: "https://soundcloud.com/zakimusicofficial/tracks?utm_source=circadiangrowth.beehiiv.com&utm_medium=newsletter&utm_campaign=melodic-mastery-4-ways-music-can-improve-our-lives",
-			},
+			want:    `[my own electronic music](https://soundcloud.com/zakimusicofficial/tracks?utm_source=circadiangrowth.beehiiv.com&utm_medium=newsletter&utm_campaign=melodic-mastery-4-ways-music-can-improve-our-lives)`,
 		},
 	}
 
@@ -36,13 +33,10 @@ func TestConvertAnchor(t *testing.T) {
 
 			got := convertAnchor(a)
 
-			if got.Text != tc.want.Text {
-				t.Errorf("convertAnchor(%s) = %s; want %s", tc.rawHTML, got.Text, tc.want.Text)
+			if got.Markdown() != tc.want {
+				t.Errorf("convertAnchor(%s) = %s; want %s", tc.rawHTML, got.Markdown(), tc.want)
 			}
 
-			if got.Href != tc.want.Href {
-				t.Errorf("convertAnchor(%s) = %s; want %s", tc.rawHTML, got.Href, tc.want.Href)
-			}
 		})
 	}
 
