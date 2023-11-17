9d788a1a4d7d1e5a07313081088d2e4969c73115
Zaki <zakislaboratory@gmail.com>
2023-11-17 16:06:02 -0600

    markdown doc + test

--- pkg/markdown/markdown.go
+++ pkg/markdown/markdown.go
@@ -3,74 +3,113 @@ package markdown
 import (
 	"fmt"
 	"regexp"
-	"strconv"
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
-// convertHtml converts HTML to a simplified Markdown format.
-func ConvertHtml(htmlContent string) (string, error) {
-	// Remove classes and styles from HTML
-	htmlContent = stripClassesFromHTML(htmlContent)
-	htmlContent = stripStylesFromHTML(htmlContent)
+type MarkdownDocument interface {
+	// Content returns the Markdown representation of the HTML document.
+	Content() (string, error)
 
-	// Load HTML content into a goquery document
-	doc, err := goquery.NewDocumentFromReader(strings.NewReader(htmlContent))
+	// RemoveMatches scans the Markdown document for any elements whose
+	// Markdown content matches the given regex pattern and removes them.
+	RemoveMatches(*regexp.Regexp)
+}
+
+type document struct {
+	// The raw HTML content to convert to Markdown.
+	html string
+
+	// Patterns to remove from the Markdown document.
+	// Useful for removing unwanted content from the Markdown document.
+	removePatterns []*regexp.Regexp
+}
+
+func (md *document) Content() (string, error) {
+
+	doc, err := goquery.NewDocumentFromReader(strings.NewReader(md.html))
 	if err != nil {
-		return "", err
+		return "", fmt.Errorf("error loading HTML content into goquery document: %v", err)
 	}
-	// Convert HTML elements to Markdown
-	var markdownContent string
-	doc.Find("body *").Each(func(_ int, s *goquery.Selection) {
-		switch s.Get(0).Data {
-		case "p":
-			markdownContent += "\n" + s.Text() + "\n"
-		case "h1", "h2", "h3", "h4", "h5", "h6":
-			level, _ := strconv.Atoi(s.AttrOr("level", "2"))
-			markdownContent += fmt.Sprintf("\n%s %s\n", strings.Repeat("#", level), s.Text())
-		case "a":
-			href, _ := s.Attr("href")
-			markdownContent += fmt.Sprintf("[%s](%s)", s.Text(), href)
-		case "ul":
-			markdownContent += "\n"
-			s.Find("li").Each(func(_ int, li *goquery.Selection) {
-				markdownContent += fmt.Sprintf("* %s\n", li.Text())
-			})
-			markdownContent += "\n"
-		case "ol":
-			markdownContent += "\n"
-			s.Find("li").Each(func(i int, li *goquery.Selection) {
-				markdownContent += fmt.Sprintf("%d. %s\n", i+1, li.Text())
-			})
-			markdownContent += "\n"
-		default:
-			// markdownContent += "\n" + s.Text() + "\n"
+
+	var content string
+
+	doc.Find("body").Children().Each(func(_ int, s *goquery.Selection) {
+
+		// Convert the selection to Markdown
+		markdownContent := convertSelection(s)
+
+		// If the selection should be removed, skip it
+		if md.shouldRemove(markdownContent) {
+			return
 		}
+
+		// Add the Markdown content to the document
+		content += markdownContent + "\n\n"
 	})
 
-	// Define the regex pattern for the "Powered by beehiiv" line
-	poweredByBeehiivPattern := `\[[^\]]+\]\(https:\/\/www\.beehiiv\.com\/\?.*?\)`
+	content = strings.ReplaceAll(content, "\n\n", "\n")
+	content = strings.TrimSuffix(content, "\n")
 
-	// Compile the regex pattern
-	re := regexp.MustCompile(poweredByBeehiivPattern)
-
-	// Strip "Powered by beehiiv" at the end of the content
-	markdownContent = re.ReplaceAllString(markdownContent, "")
-
-	return markdownContent, nil
+	return content, nil
 }
 
-// stripClassesFromHTML removes class attributes from HTML elements
-// i.e. : <div class='myUnknownClass'> => <div>
-func stripClassesFromHTML(htmlContent string) string {
-	re := regexp.MustCompile(` class=['"][^'"]*['"]`)
-	return re.ReplaceAllString(htmlContent, "")
+func convertSelection(s *goquery.Selection) string {
+
+	elements := make([]Element, 0)
+
+	text := s.Text()
+
+	switch s.Get(0).Data {
+	case "p":
+		elements = append(elements, NewParagraph(s))
+	case "h1", "h2", "h3", "h4", "h5", "h6":
+		elements = append(elements, NewHeading(s))
+	case "a":
+		elements = append(elements, NewLink(s))
+	case "ul", "ol":
+		elements = append(elements, NewList(s))
+	case "body":
+		// Do nothing
+	default:
+		panic(fmt.Sprintf("Unknown element: %s", s.Get(0).Data))
+	}
+
+	// If the selection has children, we have to convert it to a
+	// Markdown element, then replace the text in the parent with the
+	// child's Markdown text.
+	s.Children().Each(func(_ int, s *goquery.Selection) {
+		childMarkdown := convertSelection(s)
+		// Replace the child's text in the parent's text
+		text = strings.ReplaceAll(text, s.Text(), childMarkdown)
+	})
+
+	// Replace the selection's text with the Markdown text
+	text = strings.ReplaceAll(text, s.Text(), elements[0].Markdown())
+
+	return text
 }
 
-// stripStylesFromHTML removes style attributes from HTML elements
-// i.e. : <div style='color: red;'> or <div style="color: red;"> => <div>
-func stripStylesFromHTML(htmlContent string) string {
-	re := regexp.MustCompile(` style=['"][^'"]*['"]`)
-	return re.ReplaceAllString(htmlContent, "")
+func (md *document) shouldRemove(markdownContent string) bool {
+
+	for _, pattern := range md.removePatterns {
+
+		if pattern.MatchString(markdownContent) {
+			return true
+		}
+	}
+
+	return false
+}
+
+func (md *document) RemoveMatches(pattern *regexp.Regexp) {
+	md.removePatterns = append(md.removePatterns, pattern)
+}
+
+func NewMarkdownDocument(rawHtml string) MarkdownDocument {
+	return &document{
+		html:           rawHtml,
+		removePatterns: make([]*regexp.Regexp, 0),
+	}
 }
--- pkg/markdown/markdown_test.go
+++ pkg/markdown/markdown_test.go
@@ -1,8 +1,11 @@
 package markdown
 
-import "testing"
+import (
+	"regexp"
+	"testing"
+)
 
-func TestConvertHtml(t *testing.T) {
+func Test(t *testing.T) {
 
 	tcs := []struct {
 		name string
@@ -10,24 +13,63 @@ func TestConvertHtml(t *testing.T) {
 		want string
 	}{
 		{
-			name: "test 1",
-			in:   `<a class="link" href="https://soundcloud.com/zakimusicofficial/tracks?utm_source=circadiangrowth.beehiiv.com&utm_medium=newsletter&utm_campaign=melodic-mastery-4-ways-music-can-improve-our-lives" target="_blank" rel="noopener noreferrer nofollow">my own electronic music</a> `,
+			name: "Single Link",
+			in:   `<a class="link" href="https://soundcloud.com/zakimusicofficial/tracks?utm_source=circadiangrowth.beehiiv.com&utm_medium=newsletter&utm_campaign=melodic-mastery-4-ways-music-can-improve-our-lives" target="_blank" rel="noopener noreferrer nofollow">my own electronic music</a>`,
 			want: `[my own electronic music](https://soundcloud.com/zakimusicofficial/tracks?utm_source=circadiangrowth.beehiiv.com&utm_medium=newsletter&utm_campaign=melodic-mastery-4-ways-music-can-improve-our-lives)`,
+		},
+		{
+			name: "Nested link",
+			in:   `<p class="paragraph" style="text-align:start;">I even produce some of <a class="link" href="https://soundcloud.com/zakimusicofficial/tracks?utm_source=circadiangrowth.beehiiv.com&utm_medium=newsletter&utm_campaign=melodic-mastery-4-ways-music-can-improve-our-lives" target="_blank" rel="noopener noreferrer nofollow">my own electronic music</a> from time to time (shameless self plug).</p>`,
+			want: `I even produce some of [my own electronic music](https://soundcloud.com/zakimusicofficial/tracks?utm_source=circadiangrowth.beehiiv.com&utm_medium=newsletter&utm_campaign=melodic-mastery-4-ways-music-can-improve-our-lives) from time to time (shameless self plug).`,
 		},
 	}
 
 	for _, tc := range tcs {
 		t.Run(tc.name, func(t *testing.T) {
 
-			got, err := ConvertHtml(tc.in)
+			got := NewMarkdownDocument(tc.in)
+
+			content, err := got.Content()
 			if err != nil {
-				t.Fatalf("ConvertHtml(%s) returned error: %v", tc.in, err)
+				t.Fatalf("got.Content() returned error: %v", err)
 			}
 
-			if got != tc.want {
-				t.Errorf("parse(%s) = %s; want %s", tc.in, got, tc.want)
+			if content != tc.want {
+				t.Errorf("[%s] = %s; want %s", tc.name, content, tc.want)
 			}
 		})
 	}
 
+}
+
+func TestPatternMatches(t *testing.T) {
+
+	html := `<p>Hello world</p>
+<p>This is a complete match</p>
+<p>This is a partial match</p>
+	`
+
+	md := NewMarkdownDocument(html)
+
+	// Define the regex pattern for the "complete match" line
+	completeMatchPattern := `This is a complete match`
+	partialMatchPattern := `partial match`
+
+	// Compile the regex pattern
+	re := regexp.MustCompile(completeMatchPattern)
+	re2 := regexp.MustCompile(partialMatchPattern)
+
+	// Add the regex pattern to the list of patterns to remove
+	md.RemoveMatches(re)
+	md.RemoveMatches(re2)
+
+	content, err := md.Content()
+	if err != nil {
+		t.Fatalf("md.Content() returned error: %v", err)
+	}
+
+	// Check that the "complete match" line was removed
+	if content != "Hello world" {
+		t.Errorf("md.String() = %s; want %s", content, "Hello world")
+	}
 }
