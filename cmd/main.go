ffa8afea98358eb8cf424ca696f99d676716081d
Zaki <zakislaboratory@gmail.com>
2023-11-17 18:36:35 -0600

    script v1

--- cmd/main.go
+++ cmd/main.go
@@ -7,11 +7,10 @@ import (
 	"os"
 	"path/filepath"
 	"regexp"
-	"strconv"
 	"strings"
 	"time"
 
-	"github.com/PuerkitoBio/goquery"
+	"github.com/zakislaboratory/rss-to-mdx/pkg/markdown"
 )
 
 type RSSItem struct {
@@ -65,9 +64,14 @@ func main() {
 	// Process each RSS item
 	for _, item := range rss.Items {
 
-		content := stripClassesFromHTML(item.Content)
-		content = stripStylesFromHTML(content)
-		content, err = convertHTMLToMarkdownSimple(content)
+		md := markdown.NewDocument(item.Content)
+
+		// Remove beehiiv branding
+		beehiiv := regexp.MustCompile(`\[Powered by beehiiv\]\(([^)]+)\)`)
+
+		md.RemoveMatches(beehiiv)
+
+		content, err := md.Content()
 		if err != nil {
 			fmt.Printf("Error converting HTML to Markdown: %v\n", err)
 			return
@@ -95,7 +99,7 @@ creator: "%s"
 `, item.Title, item.Description, item.Link, item.GUID,
 			date, item.Creator, content)
 
-		// Add a line at the bottom that says "this was first published on Circadian Growth"
+		// Add any additional content to the end of the MDX file
 		mdxContent += ("\n\n---\n\nThis was originally published on my weekly newsletter [Circadian Growth](circadiangrowth.com)")
 
 		// Clean up title for filename
@@ -103,6 +107,7 @@ creator: "%s"
 
 		// Write MDX content to file
 		outputPath := filepath.Join(*outputDir, filename)
+
 		err = os.WriteFile(outputPath, []byte(mdxContent), os.ModePerm)
 		if err != nil {
 			fmt.Printf("Error writing MDX file %s: %v\n", filename, err)
@@ -110,72 +115,6 @@ creator: "%s"
 			fmt.Printf("MDX file %s created successfully\n", filename)
 		}
 	}
-}
-
-// convertHTMLToMarkdownSimple converts HTML to a simplified Markdown format.
-func convertHTMLToMarkdownSimple(htmlContent string) (string, error) {
-	// Remove classes and styles from HTML
-	htmlContent = stripClassesFromHTML(htmlContent)
-	htmlContent = stripStylesFromHTML(htmlContent)
-
-	// Load HTML content into a goquery document
-	doc, err := goquery.NewDocumentFromReader(strings.NewReader(htmlContent))
-	if err != nil {
-		return "", err
-	}
-	// Convert HTML elements to Markdown
-	var markdownContent string
-	doc.Find("body *").Each(func(_ int, s *goquery.Selection) {
-		switch s.Get(0).Data {
-		case "p":
-			markdownContent += "\n" + s.Text() + "\n"
-		case "h1", "h2", "h3", "h4", "h5", "h6":
-			level, _ := strconv.Atoi(s.AttrOr("level", "2"))
-			markdownContent += fmt.Sprintf("\n%s %s\n", strings.Repeat("#", level), s.Text())
-		case "a":
-			href, _ := s.Attr("href")
-			markdownContent += fmt.Sprintf("[%s](%s)", s.Text(), href)
-		case "ul":
-			markdownContent += "\n"
-			s.Find("li").Each(func(_ int, li *goquery.Selection) {
-				markdownContent += fmt.Sprintf("* %s\n", li.Text())
-			})
-			markdownContent += "\n"
-		case "ol":
-			markdownContent += "\n"
-			s.Find("li").Each(func(i int, li *goquery.Selection) {
-				markdownContent += fmt.Sprintf("%d. %s\n", i+1, li.Text())
-			})
-			markdownContent += "\n"
-		default:
-			// markdownContent += "\n" + s.Text() + "\n"
-		}
-	})
-
-	// Define the regex pattern for the "Powered by beehiiv" line
-	poweredByBeehiivPattern := `\[[^\]]+\]\(https:\/\/www\.beehiiv\.com\/\?.*?\)`
-
-	// Compile the regex pattern
-	re := regexp.MustCompile(poweredByBeehiivPattern)
-
-	// Strip "Powered by beehiiv" at the end of the content
-	markdownContent = re.ReplaceAllString(markdownContent, "")
-
-	return markdownContent, nil
-}
-
-// stripClassesFromHTML removes class attributes from HTML elements
-// i.e. : <div class='myUnknownClass'> => <div>
-func stripClassesFromHTML(htmlContent string) string {
-	re := regexp.MustCompile(` class=['"][^'"]*['"]`)
-	return re.ReplaceAllString(htmlContent, "")
-}
-
-// stripStylesFromHTML removes style attributes from HTML elements
-// i.e. : <div style='color: red;'> or <div style="color: red;"> => <div>
-func stripStylesFromHTML(htmlContent string) string {
-	re := regexp.MustCompile(` style=['"][^'"]*['"]`)
-	return re.ReplaceAllString(htmlContent, "")
 }
 
 // cleanFilename removes invalid characters from a string to be used as a filename
